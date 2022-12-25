0cf55b95d7478f0542d60aded1c8a446af4ab6b4
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2022-12-25 14:02:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -16,11 +16,11 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 0))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", None)  # Change None to your API_HASH
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  # Change None to your BOT_TOKEN
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))  # Change 0 to your OWNER_ID
-    OWNER_NAME = os.environ.get("OWNER_NAME", None)  # Change None to your OWNER_NAME
+    API_ID = int(os.environ.get("16246834", 0))  # Change 12345 to your API_ID
+    API_HASH = os.environ.get("29b3ffa9245c07f05375b92f38e8f13d", None)  # Change None to your API_HASH
+    BOT_TOKEN = os.environ.get("5636298682:AAFZGfJhMuUZGo98GlPvuXg-7Id3TXjh230", None)  # Change None to your BOT_TOKEN
+    OWNER_ID = int(os.environ.get("1715348447", 0))  # Change 0 to your OWNER_ID
+    OWNER_NAME = os.environ.get("cyellaku", None)  # Change None to your OWNER_NAME
 
     # For Local Deploys edit above 5 lines.
     # Put your API_ID and OWNER_ID (without comma) and API_HASH,BOT_TOKEN n OWNER_NAME (with commas) below.
